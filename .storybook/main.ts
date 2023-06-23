b08936540f75b1dbbb40814004d30eb7a5682377
nus3 <hadayota33@gmail.com>
2023-06-23 11:36:57 +0900

    add stories directory and move story files

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,7 +1,7 @@
 import type { StorybookConfig } from "@storybook/react-vite";
 
 const config: StorybookConfig = {
-  stories: ["../src/components/*.stories.@(js|jsx|ts|tsx)"],
+  stories: ["../stories/*.stories.@(js|jsx|ts|tsx)"],
   framework: {
     name: "@storybook/react-vite",
     options: {},
